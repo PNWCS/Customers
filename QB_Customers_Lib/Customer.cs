4928640e32d93bed283fd5e678443028a4f95d2e
Sai Charan Pinjala <spinjal@pnw.edu>
2025-04-01 21:00:15 +0000

    Cleared the test cases

--- QB_Customers_Lib/Customer.cs
+++ QB_Customers_Lib/Customer.cs
@@ -8,5 +8,17 @@ namespace QB_Customers_Lib
 {
     public class Customer
     {
+
+        public string Name { get; set; }
+        public string Fax { get; set; }
+
+        public string QB_ID { get; set; }
+
+        public Customer(string name, string fax)
+        {
+            Name = name;
+            Fax = fax;
+            
+        }
     }
 }
